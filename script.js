aecd877d7eb2827e4cf907a45495ec71a7456a48
Thakur Avneesh Singh <avionprithvii@icloud.com>
2026-05-06 01:24:12 +0530

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ document.addEventListener('mousemove', e => {
   cursor.style.top = e.clientY + 'px';
   cursor.style.left = e.clientX + 'px';
 });
+window.addEventListener("load", () => {
+  document.getElementById("loader").style.display = "none";
+});
